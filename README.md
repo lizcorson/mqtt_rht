4fc32ede88ec2852d94658121d80e00ac1f95481
Liz Corson <elizabethcorson@gmail.com>
2016-04-05 23:41:26 -0400

    Initial code
    Code for MQTT publishing without sensors installed yet.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # mqtt_rht
 A network of sensors to monitor the temperature in various parts of my home.
+
+## Overview
+I'd like to monitor the temperature in various parts of my home, so I'm setting up a bunch of little wifi-enabled RHT (relative humidity/temperature) sensors in various rooms. Data will be published to MQTT. I'm running an MQTT server on my Raspberry Pi. I'd also like to create a dashboard webpage to chart the data, and I'd also like to log it to disk either as a text file or maybe to a MySQL database.
+
+## Status
+I've currently written the initial code for the microcontroller board, but I have yet to install the sensors.
+
+## Parts Used
+- NodeMCU Clone http://www.aliexpress.com/item/NodeMcu-Lua-WIFI-development-board-based-on-the-ESP8266-Internet-of-things/32339203041.html - can't say I enthusiastically recommend these as the quality is somewhat questionable. But they're cheap and I needed a bunch.
+- HTU21D clone http://www.ebay.com/itm/201496730282 (original: https://www.sparkfun.com/products/12064)
+- Raspberry Pi 3 - Jessie Lite
+
+## Libraries
+- PubSubClient https://github.com/knolleary/pubsubclient
+- HTU21D https://github.com/sparkfun/HTU21D_Breakout/tree/V_H1.2_L1.1.0/Libraries/Arduino
+
+## Other
+
+Follow this tutorial to install Mosquitto on your Raspberry Pi from the Debian repositories, which will let you access a much newer version: http://mosquitto.org/2013/01/mosquitto-debian-repository/
+
+I like the apps MQTT Dashboard and MyMQTT, which are free on Google Play for Android. These can be helpful for troubleshooting.
---
+++ mqtt_rht/mqtt_rht.ino
@@ -0,0 +1,122 @@
+/* mqtt_rht
+ *  Publish temperature/humidity sensor data with MQTT. Based on ESP8266 MQTT example.
+ *
+ * Liz Corson
+ * 5 April 2016
+ */
+
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "SparkFunHTU21D.h"
+
+// Update these with values suitable for your network.
+
+const char* ssid = "...";
+const char* password = "...";
+const char* mqtt_server = "...";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+const int msgDelay = 5000; //how often to send data
+char msg_rh[50];
+char msg_t[50];
+HTU21D myHumidity;
+
+void setup() {
+  Wire.begin();
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  myHumidity.begin();
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("inTopic");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  if ((unsigned long)(millis() - lastMsg) >= msgDelay) {
+    lastMsg = millis();
+
+    //I don't have the sensors wired up yet so I'm temporarily generating random values.
+
+    //float temp = myHumidity.readTemperature()*1.8+32; //convert to F
+    //float rh = myHumidity.readHumidity();
+
+    float temp = random(-4000,10000)/100.0;
+    float rh = random(0,10000)/100.0;
+
+    Serial.println(temp);
+    Serial.println(rh);
+
+    String rh_str = String(rh);
+    rh_str.toCharArray(msg_rh, rh_str.length() + 1);
+    Serial.print("Publish message: ");
+    Serial.println(msg_rh);
+    client.publish("office/rh", msg_rh);
+
+    String temp_str = String(temp);
+    temp_str.toCharArray(msg_t, temp_str.length() + 1);
+    Serial.print("Publish message: ");
+    Serial.println(msg_t);
+    client.publish("office/temp", msg_t);
+
+    //Thanks to this post: http://forum.arduino.cc/index.php?topic=241222.0
+  }
+}
